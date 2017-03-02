68ff571215b0cd8e154161c565b7b50f554b1e9f
Michael Flynn <flynnmj@devnoise.com>
2017-03-02 19:59:51 +0000

    code formatting.

--- server.js
+++ server.js
@@ -2,19 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  console.log(req.headers);
-    console.log(req.ip);
-  console.log(req.connection);
-    var output = {
-      "ipaddress": req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-      "language": req.headers['accept-language'],
-      "software": req.headers['user-agent'].split('(')[1].split(')')[0]
-    };
-        res.end(JSON.stringify(output));
-
+  var output = {
+    "ipaddress": req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent'].split('(')[1].split(')')[0]
+  };
+  res.end(JSON.stringify(output));
 });
 
 app.listen(8080, function() {
   console.log('Example app listening on port 8080!')
 });
-
